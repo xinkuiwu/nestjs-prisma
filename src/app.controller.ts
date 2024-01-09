873fe6b471dd15ab3707cbcc8ec6bb0200650e9b
wuxinkui <wuxinkui@git>
2024-01-09 09:40:51 +0800

    feat: nestjs-prisma

---
+++ src/app.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { AppService } from './app.service';
+import { DepartmentService } from './department.service';
+import { EmployeeService } from './employee.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Inject(DepartmentService)
+  private departmentService: DepartmentService
+
+  @Inject(EmployeeService)
+  private employeeService: EmployeeService
+
+  @Get('create')
+  async create() {
+    const department = await this.departmentService.create({
+      name: '技术部'
+    })
+
+    await this.employeeService.create({
+      name: '张三',
+      phone: '1322222222',
+      department: {
+        connect: {
+          id: department.id
+        }
+      }
+    })
+
+    return 'done'
+  }
+
+}
